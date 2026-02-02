d498d5b5a7f08061368738be937f539926f4cf63
lei <lei@wikiglobal.com>
2026-02-02 16:34:42 +0800

    feat: update

--- backend/src/stats/stats.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StatsController } from './stats.controller';
-import { StatsService } from './stats.service';
-
-@Module({
-  controllers: [StatsController],
-  providers: [StatsService],
-})
-export class StatsModule {}
--- backend/src/view/view.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ViewController } from './view.controller';
-import { LotteryModule } from '../lottery/lottery.module';
-
-@Module({
-  imports: [LotteryModule],
-  controllers: [ViewController],
-})
-export class ViewModule {}
